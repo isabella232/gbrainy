c60bf7af2eb34776fb7e6ec4145d046120be82df
Daniel Mustieles <daniel.mustieles@gmail.com>
2014-01-31 14:59:14 +0100

    Revert "Updated FSF's address"
    This reverts commit 611850606a0b40db6d3cf36f3b6bdb3a357b2604.

--- src/Clients/Classical/Dialogs/AboutDialog.cs
+++ src/Clients/Classical/Dialogs/AboutDialog.cs
@@ -12,7 +12,60 @@
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program; if not, see <http://www.gnu.org/licenses/>."));
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+using System;
+using Gtk;
+using Gdk;
+using Mono.Unix;
+using System.Text;
+
+namespace gbrainy.Clients.Classical.Dialogs
+{
+	public class AboutDialog : Gtk.AboutDialog
+	{
+		public AboutDialog () : base ()
+		{
+			StringBuilder license = new StringBuilder (256);
+			string [] authors = new string [] {
+				Catalog.GetString ("Software"),
+				"  Jordi Mas i Hernandez <jmas@softcatala.org>",
+				"",
+				Catalog.GetString ("Based on ideas by"),
+				"  Terry Stickels",
+				"  Jordi Mas i Hernandez",
+				"  Lewis Carroll",
+				("  " + Catalog.GetString ("MENSA works")),
+				"  mathforum.org",
+				"  Teun Spaans"
+			};
+
+			string [] artists = new string [] {
+				"Anna Barber\u00e0 Mar\u00e9",
+				"Carme Cabal Sard\u00e0",
+				"Jordi Mas i Hernandez",
+				"Felipe Menegaz",
+				"John Cliff",
+				"Openclipart.org",
+			};
+
+			string [] documenters = new string [] {
+				"Milo Casagrande <milo@ubuntu.com>",
+				"Jordi Mas i Hernandez <jmas@softcatala.org>"
+			};
+
+			// Translators: Replace by the name of the people that translated the application
+			string translators = Catalog.GetString ("translator-credits");
+
+			if (translators == "translator-credits")
+				translators = null;
+
+			license.Append (Catalog.GetString ("This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as  published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\n\n"));
+			license.Append (Catalog.GetString ("This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\n"));
+			license.Append (Catalog.GetString ("You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA."));
 
 			ProgramName = "gbrainy";
 			Version = Defines.VERSION;
