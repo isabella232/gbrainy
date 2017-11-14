60946738afa6f476752c61ee4482a7bbea9da161
Anders Jonsson <anders.jonsson@norsjovallen.se>
2017-11-14 22:22:20 +0100

    #Closes 753045 - Obsolete FSF address is used

--- src/Clients/Classical/Dialogs/AboutDialog.cs
+++ src/Clients/Classical/Dialogs/AboutDialog.cs
@@ -12,9 +12,7 @@
  * General Public License for more details.
  *
  * You should have received a copy of the GNU General Public
- * License along with this program; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * License along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
@@ -65,7 +63,7 @@ namespace gbrainy.Clients.Classical.Dialogs
 
 			license.Append (Catalog.GetString ("This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as  published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.\n\n"));
 			license.Append (Catalog.GetString ("This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\n"));
-			license.Append (Catalog.GetString ("You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA."));
+			license.Append (Catalog.GetString ("You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>."));
 
 			ProgramName = "gbrainy";
 			Version = Defines.VERSION;
