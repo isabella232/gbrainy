062a6cd9f590046ca19ee74e3552c61be3a0989d
Jordi Mas <jmas@softcatala.org>
2019-05-25 19:14:42 +0200

    Application class name to match application

--- tools/TranslationsChecker.cs
+++ tools/TranslationsChecker.cs
@@ -23,7 +23,7 @@ using System.Collections.Generic;
 using MonoDevelop.Gettext;
 using System.Text.RegularExpressions;
 
-public class GameXmlToGetString
+public class TranslationsChecker
 {
 	public class Parser : CatalogParser
 	{
